1f50bacbbccc0ab4d7117bd19afa8d7f2698aaee
Luis Alexander Aldazabal Gil <berczeck_400@hotmail.com>
2015-04-04 22:34:10 -0500

    Codigo inicial code coverage

---
+++ CodeCoverage/CodeCoverage/TrianguloTest.cs
@@ -0,0 +1,38 @@
+﻿using Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeCoverage
+{
+    [TestClass]
+    public class TrianguloTest
+    {
+[TestMethod]
+        public void Validar_AngulosMenoresCero_RetornaTrianguloInvalido()
+{
+    //Arrange
+    var triangulo = new Triangulo();
+
+    //Act
+    var resultado = triangulo.Validar(0, 0, 0);
+
+    //Assert
+    Assert.AreEqual("Triangulo Inválido", resultado);
+}
+
+
+[TestMethod]
+public void Validar_AngulosInvalidos_RetornaTrianguloInvalido()
+{
+    //Arrange
+    var triangulo = new Triangulo();
+
+    //Act
+    var resultado = triangulo.Validar(17, 8, 8);
+
+    //Assert
+    Assert.AreEqual("Triangulo Inválido", resultado);
+}
+
+
+    }
+}
---
+++ CodeCoverage/Core/Triangulo.cs
@@ -0,0 +1,30 @@
+﻿namespace Core
+{
+    public class Triangulo
+    {
+        public string Validar(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                return "Triangulo Inválido";
+            }
+
+            if ((a > b + c || b > a + c || c > a + b))
+            {
+                return "Triangulo Inválido";
+            }
+
+            if (a == b && b == c)
+            {
+                return "Triangulo Equilátero";
+            }
+
+            if (a == b || b == c || a == c)
+            {
+                return "Triangulo Isósceles";
+            }
+
+            return "Triangulo  Escaleno";
+        }
+    }
+}
